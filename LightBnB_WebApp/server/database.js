6e353f16eb1ee0b420abc0d4b685f0ce7b291b4f
Kevin Mason <krmason2020@gmail.com>
2021-11-25 22:15:52 +0000

    completed  getAllReservations database function, also added 2 properties to user 1's future reservations

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -73,12 +73,6 @@ const addUser = function (user) {
       console.log("###error:", err);
       return null;
     });
-
-  console.log(user);
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
 };
 exports.addUser = addUser;
 
@@ -90,7 +84,27 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function (guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool
+    .query(
+      `SELECT properties.*, reservations.*, avg(rating) as average_rating
+      FROM reservations
+      JOIN properties ON reservations.property_id = properties.id
+      JOIN property_reviews ON properties.id = property_reviews.property_id
+      WHERE reservations.guest_id = $1
+      AND reservations.end_date > now()::date
+      GROUP BY properties.id, reservations.id
+      ORDER BY reservations.start_date
+      LIMIT $2`,
+      [guest_id, limit]
+    )
+    .then((result) => {
+      console.log(result.rows);
+      return result.rows;
+    })
+    .catch((err) => {
+      console.log("###error:", err);
+      return null;
+    });
 };
 exports.getAllReservations = getAllReservations;
 
